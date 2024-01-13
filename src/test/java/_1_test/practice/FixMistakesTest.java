ba556ac7d553818bec2dc277fd34eb83702aa8a7
stanislaumalyshau <stanislav.malyshau@moneyman.kz>
2024-01-13 11:34:30 +0100

    feat(test): polish comments

--- src/test/java/_1_test/practice/FixMistakesTest.java
+++ src/test/java/_1_test/practice/FixMistakesTest.java
@@ -5,23 +5,31 @@ import org.junit.jupiter.api.Test;
 
 public class FixMistakesTest {
 
-    // TODO: fix reason why this test doesn't start
+    /**
+     * TODO: fix reason why this test doesn't start
+     */
     @Test
     private void test() {
     }
 
-    // TODO: fix reason why this test doesn't start¬
+    /**
+     * TODO: fix reason why this test doesn't start¬
+     */
     @Test
     void calculate(int x) {
     }
 
-    // TODO: make this test green.
+    /**
+     * TODO: make this test green.
+     */
     @Test
     void assertionTest() {
         Assertions.assertFalse(true);
     }
 
-    // TODO: make this test green
+    /**
+     * TODO: make this test green
+     */
     @Test
     void calculationMistake() {
         Assertions.assertTrue(3 > 5 + 1);
