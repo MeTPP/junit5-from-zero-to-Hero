ff9dfefa893d5e1c51320e4618cff45aa1c11aa8
stanislaumalyshau <stanislav.malyshau@moneyman.kz>
2024-01-13 13:01:50 +0100

    feat(tempDir):  added tasks to TempDirPracticeTest

--- src/test/java/_20_built_in_extension/practice/TempDirPracticeTest.java
+++ src/test/java/_20_built_in_extension/practice/TempDirPracticeTest.java
@@ -1,4 +1,41 @@
 package _20_built_in_extension.practice;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 public class TempDirPracticeTest {
+
+
+    @TempDir
+    Path tempDir;
+
+    /**
+     * TODO using tempDir write 123 to file then read it and assert it.
+     */
+    @Test
+    void writeToFile() {
+
+    }
+
+
+    /**
+     * FIXME: make this test green. fix bug.
+     */
+    @Test
+    void copyFileFromSourceToTarget(@TempDir Path source, @TempDir Path target) throws IOException {
+        Path sourceFile = source.resolve("test.txt");
+        Files.writeString(target, "abc");
+        Path targetFile = Files.copy(sourceFile, target.resolve("test.txt"));
+
+        Assertions.assertNotEquals(sourceFile, targetFile);
+        Assertions.assertEquals(List.of("abc"), Files.readAllLines(targetFile));
+    }
+
+
 }
