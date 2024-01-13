e2ee0e530e90b9f97cef67c93606a4f3d9a0f9cf
stanislaumalyshau <stanislav.malyshau@moneyman.kz>
2024-01-13 12:42:51 +0100

    feat(parametrized):  added tasks to EnumSourcePracticeTest

--- src/test/java/_14_parametrized_test/practice/EnumSourcePracticeTest.java
+++ src/test/java/_14_parametrized_test/practice/EnumSourcePracticeTest.java
@@ -1,5 +1,45 @@
 package _14_parametrized_test.practice;
 
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+
 public class EnumSourcePracticeTest {
+
+
+    /*
+     * TODO: create test that will run for each value in Status enum
+     */
+
+
+    /**
+     * FIXME: we need only IN_PROGRESS and END values to run. change accordingly.
+     */
+    @ParameterizedTest
+    @EnumSource(mode = EnumSource.Mode.EXCLUDE, names = {"IN_PROGRESS", "END"}, value = Status.class)
+    void test1() {
+
+    }
+
+    /*
+     *  TODO: create test that will start test only once. It should start with value which contains E.
+     */
+
+
+    /**
+     * FIXME: change name param. we need to run tests only for enums that starts with "START"
+     */
+    @ParameterizedTest
+    @EnumSource(mode = EnumSource.Mode.MATCH_ALL, names = "START", value = Status.class)
+    void test() {
+
+    }
+
+
+    enum Status {
+        START,
+        START_PREPARATION,
+        IN_PROGRESS,
+        END
+    }
 
 }
