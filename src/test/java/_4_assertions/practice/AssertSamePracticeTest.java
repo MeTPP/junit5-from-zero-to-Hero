61ecb5d29d20bf2b86106f546461a68d1982d141
stanislaumalyshau <stanislav.malyshau@moneyman.kz>
2024-01-13 11:58:29 +0100

    feat(assertions): added AssertSamePracticeTest tasks

--- src/test/java/_4_assertions/practice/AssertSamePracticeTest.java
+++ src/test/java/_4_assertions/practice/AssertSamePracticeTest.java
@@ -1,4 +1,40 @@
 package _4_assertions.practice;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class AssertSamePracticeTest {
+
+
+    /**
+     * TODO: change assertion to make test green
+     */
+    @Test
+    void testObjectsSame(){
+        Assertions.assertSame(new Object(), new Object());
+    }
+
+
+    /**
+     * TODO: using  Assertions.assertNotSame/ Assertions.assertSame
+     * assert if this objects same
+     */
+    @Test
+    void assertSame() {
+        var expected = Integer.valueOf(12);
+        var actual = Integer.valueOf(12);
+        Assertions.assertSame(expected, actual);
+    }
+
+    /**
+     * TODO: using  Assertions.assertNotSame/ Assertions.assertSame
+     * assert if this objects same
+     */
+    @Test
+    void assertNotSame() {
+        var expected = Integer.valueOf(1000);
+        var actual = Integer.valueOf(1000);
+        Assertions.assertNotSame(expected, actual);
+    }
+
 }
