7615e9f258954f9a9918df7e2565df02db479244
stanislaumalyshau <stanislav.malyshau@moneyman.kz>
2024-01-13 11:36:33 +0100

    feat(assertions): polish comments

--- src/test/java/_4_assertions/practice/AssertAllPracticeTest.java
+++ src/test/java/_4_assertions/practice/AssertAllPracticeTest.java
@@ -7,22 +7,27 @@ import java.util.stream.Stream;
 
 public class AssertAllPracticeTest {
 
-
+    /**
+     * TODO:  add heading to assertion
+     */
     @Test
     void assertAllWithHeading() {
-        // add heading to assertion
         Assertions.assertAll(() -> Assertions.assertTrue(true));
     }
 
-    //combine two assertion
-    //Assertions.assertTrue(true) and Assertions.assertFalse(false) with assertAll
+    /**
+     * TODO: combine two assertion
+     * Assertions.assertTrue(true) and Assertions.assertFalse(false) with assertAll
+     */
     @Test
     void assertAll() {
         Assertions.assertAll();
     }
 
 
-    // group these assertions under one assertAll
+    /**
+     * TODO: group these assertions under one assertAll
+     */
     @Test
     void fixAssertAll() {
         Assertions.assertAll(Stream.of(() -> Assertions.assertTrue(true)));
