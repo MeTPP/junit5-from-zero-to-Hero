32c55cbef1c045c2dbffad824d33fb0a28b3a517
stanislaumalyshau <stanislav.malyshau@moneyman.kz>
2024-01-14 19:40:09 +0100

    feat(disabled): polish disable practice folder

--- src/test/java/_6_disabling_tests/practice/DisableMethodPracticeTest.java
+++ src/test/java/_6_disabling_tests/practice/DisableMethodPracticeTest.java
@@ -2,11 +2,16 @@ package _6_disabling_tests.practice;
 
 public class DisableMethodPracticeTest {
 
+    /*
+     * TODO: write test that will pass successfully
+     */
 
-    //TODO: write test that will pass successfully
+    /*
+     * TODO: write test that will fail
+     */
 
-    //TODO: write test that will fail
-
-    //TODO: disable red test
+    /*
+     * TODO: disable red test
+     */
 
 }
