fe7600d8d7beae0bccb1e57d0d343e260653c8f0
stanislaumalyshau <stanislav.malyshau@moneyman.kz>
2024-01-15 20:43:08 +0100

    feat(nested): docs polished

--- src/test/java/_10_nested/practice/NestedPractice.java
+++ src/test/java/_10_nested/practice/NestedPractice.java
@@ -2,11 +2,14 @@ package _10_nested.practice;
 
 public class NestedPractice {
 
-    //TODO: add @BeforeAll method lifecycle to enclosing class
-    // create two nested classes:
-    // FirstNestedClass and SecondNestedClass
-    // each should contain another nested class (third and fourth respectively)
-    // and each should have @BeforeEach and @AfterEach lifecycle methods
+    /*
+     * TODO: add @BeforeAll method lifecycle to enclosing class
+     *  create two nested classes:
+     *  FirstNestedClass and SecondNestedClass
+     *  each should contain another nested class (third and fourth respectively)
+     *  and each should have @BeforeEach
+     *  and @AfterEach lifecycle methods
+     */
 
 
 }
