9930ecb690fdb60ccb502b9927b24098b41ea9c5
stanislaumalyshau <stanislav.malyshau@moneyman.kz>
2024-01-15 20:41:28 +0100

    feat(test execution order): folder polished. added tasks.

---
+++ src/test/java/_8_test_execution_order/practice/MethodOrderExampleTestCase.java
@@ -0,0 +1,46 @@
+package _8_test_execution_order.practice;
+
+public class MethodOrderExampleTestCase {
+
+    /*
+     * TODO: create nested class with MethodOrderer.DisplayName
+     *  and two methods with @display
+     */
+
+    /*
+     * TODO: create nested class with MethodOrderer.MethodName
+     *  and two methods
+     */
+
+    /*
+     * TODO: create nested class with MethodOrderer.OrderAnnotation
+     *  and two methods with @Order
+     */
+
+    /*
+     * TODO: create nested class with MethodOrderer.Random
+     *  and two methods
+     */
+
+    /*
+     * TODO: create nested class with MethodOrderer.Alphanumeric
+     *  and two methods
+     */
+
+
+    /*
+     *   TODO:  create nested class with you custom method orderer with following logic:
+     *     if testMethod name contains number than it has higher priority than method without number in name
+     */
+
+    /*
+     * TODO: create nested class
+     *  and two methods with @Order
+     *  don't use MethodOrderer.OrderAnnotation
+     *  go to file: resources/junit-platform.properties
+     *  remove leading _ from _junit.jupiter.testclass.order.default
+     *  and run test again
+     */
+
+}
+
