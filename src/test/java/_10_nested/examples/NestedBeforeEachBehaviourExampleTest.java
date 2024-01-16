357f0c2a8dc66847faa956e1163bd31b3b9a8307
stanislaumalyshau <stanislav.malyshau@moneyman.kz>
2024-01-16 12:26:46 +0100

    feat(nested): tasks and examples created

--- src/test/java/_10_nested/examples/NestedBeforeEachBehaviourExampleTest.java
+++ src/test/java/_10_nested/examples/NestedBeforeEachBehaviourExampleTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-public class BeforeEachBehaviourNestedExampleTest {
+public class NestedBeforeEachBehaviourExampleTest {
 
     @BeforeEach
     void setup() {
--- src/test/java/_10_nested/practice/NestedPracticePart2Test.java
+++ src/test/java/_10_nested/practice/NestedPracticePart2Test.java
@@ -1,8 +1,47 @@
 package _10_nested.practice;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 public class NestedPracticePart2Test {
 
-//  TODO: create nested class and two method inside it
-//    tag both of these methods using only one annotation
+    /*
+     *  TODO: create nested class and two method inside it
+     *   tag both of these methods using only one annotation
+     */
+
+
+    int x;
+
+    @BeforeEach
+    void setUp() {
+        x = 12;
+    }
+
+
+    /**
+     * TODO: make _10_nested.practice.NestedPracticePart2Test.FirstClass#test()
+     *  and _10_nested.practice.NestedPracticePart2Test.SecondClass#test() at the same time.
+     */
+    @Nested
+    class FirstClass {
+        @Test
+        void test() {
+            Assertions.assertEquals(0, x);
+        }
+
+    }
+
+    @Nested
+    class SecondClass {
+        @Test
+        void test() {
+            Assertions.assertEquals(5, x);
+
+        }
+    }
+
 
 }
