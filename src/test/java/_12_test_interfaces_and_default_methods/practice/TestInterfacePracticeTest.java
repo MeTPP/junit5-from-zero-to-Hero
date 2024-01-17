93782dc204cfec9fb13a788f23487c944fb94d6b
stanislaumalyshau <stanislav.malyshau@moneyman.kz>
2024-01-17 17:56:36 +0100

    feat(test_interfaces):  practice added.

--- src/test/java/_12_test_interfaces_and_default_methods/practice/TestInterfacePracticeTest.java
+++ src/test/java/_12_test_interfaces_and_default_methods/practice/TestInterfacePracticeTest.java
@@ -2,4 +2,11 @@ package _12_test_interfaces_and_default_methods.practice;
 
 public class TestInterfacePracticeTest {
 
+    /*
+     * TODO: create interface and methods annotated with:
+     *  @BeforeAll @BeforeEach @Test @AfterEach @AfterAll
+     *  then create nested class, implement your interface and run test.
+     */
+
+
 }
