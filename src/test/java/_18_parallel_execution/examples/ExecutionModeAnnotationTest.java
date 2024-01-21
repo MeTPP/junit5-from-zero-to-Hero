a43809fe5ab7611bccffd2d2809908760db8f855
stanislaumalyshau <stanislav.malyshau@moneyman.kz>
2024-01-21 15:12:19 +0100

    feat(parallel_execution): docs polished

--- src/test/java/_18_parallel_execution/examples/ExecutionModeAnnotationTest.java
+++ src/test/java/_18_parallel_execution/examples/ExecutionModeAnnotationTest.java
@@ -7,8 +7,29 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 import org.junit.jupiter.api.parallel.ExecutionMode;
 
+
+/**
+ * The @Execution annotation is used to configure the execution mode of the test methods within a test class in JUnit 5.
+ * JUnit 5 supports two modes of execution: SAME_THREAD and CONCURRENT.
+ * to enable parallel execution you need to set junit.jupiter.execution.parallel.enabled=true
+ * in resources/junit-platform.properties
+ */
 @Execution(ExecutionMode.CONCURRENT)
 public class ExecutionModeAnnotationTest {
+
+
+    /**
+     * CONCURRENT: With CONCURRENT execution mode, test methods are executed in different threads
+     * which allows for parallel execution.
+     * This mode is beneficial for speeding up the overall test execution time,
+     * especially for tests that are independent of each other and do not share state.
+     */
+    @Execution(ExecutionMode.CONCURRENT)
+    @Test
+    void test0() throws InterruptedException {
+        Thread.sleep(2000);
+
+    }
 
     @Test
     void test1() throws InterruptedException {
@@ -22,10 +43,16 @@ public class ExecutionModeAnnotationTest {
         Thread.sleep(2000);
     }
 
+    /**
+     * SAME_THREAD: When tests are executed with the SAME_THREAD mode,
+     * all test methods in the test class are run in the same thread.
+     * This mode is useful when you need to ensure that the test methods are not executed concurrently,
+     * for instance, when they share state that is not thread-safe.
+     */
     @RepeatedTest(2)
     @Execution(ExecutionMode.SAME_THREAD)
     void test3(RepetitionInfo repetitionInfo) throws InterruptedException {
-        System.out.printf("test3: repetirion: %d uses %s%n", repetitionInfo.getCurrentRepetition(), Thread.currentThread().getName());
+        System.out.printf("test3: repetition: %d uses %s%n", repetitionInfo.getCurrentRepetition(), Thread.currentThread().getName());
         Thread.sleep(2000);
     }
 
