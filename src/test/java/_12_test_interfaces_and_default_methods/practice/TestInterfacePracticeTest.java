0ea1092d51c916488777263e974f0460b704dfc6
stanislaumalyshau <stanislav.malyshau@moneyman.kz>
2024-02-01 17:24:28 +0100

    feat(all):practice solved

--- src/test/java/_12_test_interfaces_and_default_methods/practice/TestInterfacePracticeTest.java
+++ src/test/java/_12_test_interfaces_and_default_methods/practice/TestInterfacePracticeTest.java
@@ -1,5 +1,50 @@
 package _12_test_interfaces_and_default_methods.practice;
 
+import org.junit.jupiter.api.*;
+
 public class TestInterfacePracticeTest {
+
+    /*
+     * TODO: create interface and methods annotated with:
+     *  @BeforeAll @BeforeEach @Test @AfterEach @AfterAll
+     *  then create nested class, implement your interface and run test.
+     */
+
+    @Nested
+    class NestedClass implements DefaultExampleTest {
+
+    }
+
+    @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+    interface DefaultExampleTest {
+
+
+        @BeforeAll
+        default void beforeAll() {
+            System.out.println("beforeAll");
+        }
+
+        @BeforeEach
+        default void setUp() {
+            System.out.println("BeforeEach");
+        }
+
+        @Test
+        default void name() {
+            System.out.println("test");
+        }
+
+        @AfterEach
+        default void tearDown() {
+            System.out.println("AfterEach");
+
+        }
+
+        @AfterAll
+        static void afterAll() {
+            System.out.println("afterAll");
+
+        }
+    }
 
 }
