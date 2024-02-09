0850658c60dfcaeb825c9c06a59cd6b8213fad45
stanislaumalyshau <stanislav.malyshau@moneyman.kz>
2024-02-09 18:31:39 +0100

    feat(all): created junit folder.

--- src/test/java/_2_tagging_and_filtering/practice/GradleFilterTest.java
+++
@@ -1,9 +0,0 @@
-package _2_tagging_and_filtering.practice;
-
-public class GradleFilterTest {
-
-    /*
-     *   TODO write test with @Tag("fast") - find way to run this test (without excludeTags removal)
-     */
-
-}
--- src/test/java/_2_tagging_and_filtering/practice/MultipleTagsTest.java
+++
@@ -1,8 +0,0 @@
-package _2_tagging_and_filtering.practice;
-
-public class MultipleTagsTest {
-
-    /*
-     * TODO: create test with tags: ui, end-to-end, integration, mocks
-     */
-}
